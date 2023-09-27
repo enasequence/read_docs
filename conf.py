58b1b78c986cce7ecf7d3bbb94050cfadb62bd41
Jasmine Elaine Mckinnon <58950226+jas-mckin@users.noreply.github.com>
2023-09-27 17:07:31 +0100

    fix config
    This commit fixes the config and edits the commit 44dc80f which contains the most complete up to date docs version

--- conf.py
+++ conf.py
@@ -30,6 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+extensions = ['sphinx_tabs.tabs','myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -160,12 +161,7 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
 }
-
-source_suffix = ['.rst', '.md']
-
-extensions = ['sphinx_tabs.tabs', 'myst_parser']
