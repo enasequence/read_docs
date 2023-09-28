bd08ba1bba3974290318b1a2a65dc1ca48ef198a
Jasmine Elaine Mckinnon <58950226+jas-mckin@users.noreply.github.com>
2023-09-28 09:59:56 +0100

    fix config, fix links
    -fixes config by adding sphinx-rtd-theme and unpinning docutils from <0.17
    -fixes links to remove .html extension to internal docs links which were not working

--- conf.py
+++ conf.py
@@ -30,6 +30,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+extensions = ['sphinx_tabs.tabs', 'myst_parser']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -82,6 +84,10 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
+# Jasmine addition: please note that the theme is now specified in the requirements file as sphinx_rtd_theme
+# Jasmine addition: please note that any docs breaking could be due to a required package changing
+# Jasmine addition: it is reccommended to pin requirments - see https://docs.readthedocs.io/en/stable/guides/rep>
+html_theme = "sphinx_rtd_theme"
 
 # marc addition: from http://stackoverflow.com/questions/18969093/how-to-include-the-toctree-in-the-sidebar-of-each-page
 # remove if causing issues. forces globaltoc.html
